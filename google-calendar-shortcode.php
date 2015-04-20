f11b4aaf47834f4f44355169db8788640a8a1fe3
Eli Madden <emadden@middlebury.edu>
2015-04-20 15:40:06 -0400

    Error display for invalid ID, color, viewmode

--- google-calendar-shortcode.php
+++ google-calendar-shortcode.php
@@ -47,8 +47,10 @@ function gcs_calendar( $atts ){
 	if( isset( $atts[ 'viewmode' ] ) ){
 		//echo'<p>Viewmode before:' . $atts[ 'viewmode' ] . '</p>';
 		$viewmode = strtoupper( $atts[ 'viewmode' ] );
-		$viewmode = in_array( $viewmode, array( 'WEEK', 'MONTH', 'AGENDA' ) ) ? $viewmode : 'MONTH';
-		//echo'<p>Viewmode after:' . $viewmode . '</p>';
+		if( !in_array( $viewmode, array( 'WEEK', 'MONTH', 'AGENDA' ) ) ) {
+			$viewmode = 'MONTH';
+			$errors[] = "Invalid viewmode. Using default.";
+		}
 		$iframe .= 'mode=' . $viewmode . '&amp;';
 	}
 	if( isset( $atts[ 'height' ] ) ) $iframe .= 'height=' . $atts[ 'height' ] . '&amp;';
@@ -66,15 +68,20 @@ function gcs_calendar( $atts ){
 	//do each of the given calendar ID(s) and match with the given color(s)
 	$count = 0;
 	foreach( $id_array as $id ){ 
-		if( $count ) $iframe .= '&amp;';		
-		$iframe .= 'src=' . trim( $id );		
-		if( isset( $color_array[ $count ] ) ){
-			$color = trim( $color_array[ $count ], " #\t\n\r\0\x0B" ); //strip off '#' as well as the usual space, etc
-			$color = str_replace( '%23', '', $color ); //if user copy-pasted the %23 from Google, strip it off as well
-			$color = ( preg_match( '/^[a-f0-9]{3}$|^[a-f0-9]{6}$/i', $color ) ) ? $color : '2952A3'; //check to be sure it's a hex code, if not use default Blue. *Note: Google at this time appears to only support color codes that match the choices they give.
-			$iframe .= '&amp;color=%23' . $color;
-		}
-		$count++;
+		if( strpos( $id, 'google.com' ) > 10 ){
+			if( $count ) $iframe .= '&amp;';		
+			$iframe .= 'src=' . trim( $id );		
+			if( isset( $color_array[ $count ] ) ) {
+				$color = trim( $color_array[ $count ], " #\t\n\r\0\x0B" ); //strip off '#' as well as the usual space, etc
+				$color = str_replace( '%23', '', $color ); //if user copy-pasted the %23 from Google, strip it off as well
+				if( preg_match( '/^[a-f0-9]{3}$|^[a-f0-9]{6}$/i', $color ) ) $iframe .= '&amp;color=%23' . $color;
+				else {
+					$errors[] = 'Invalid Color Code. Using default.';
+					$iframe .= '&amp;color=%232952A3';
+				}
+			}
+			$count++;
+		}else $errors[] = "Invalid or missing Google Calendar ID";
 	}
 	if( isset( $atts[ 'timezone' ] ) ){
 		$timezone = str_replace( '%2F', '/', $atts[ 'timezone' ] ); //if user copy-pasted the %2F, convert it to a / for now so we can keep the array more readable
@@ -82,19 +89,30 @@ function gcs_calendar( $atts ){
 		else $errors[] = 'Invalid Timezone';
 	}
 	$iframe .= '"';
+	
 	if( isset( $atts[ 'show_border' ] ) && in_array( strtoupper( $atts[ 'show_border' ] ), array( '1', 'YES', 'TRUE' ) ) ) $iframe .= ' style=" border:solid 1px #777 "';
-	if( isset( $atts[ 'width' ] ) )$iframe .= ' width="' . $atts[ 'width' ] . '"';
-	if( isset( $atts[ 'height' ] ) ) $iframe .= ' height="' . $atts[ 'height' ] . '"';
-
+	$iframe .= ( isset( $atts[ 'width' ] ) ) ? ' width="' . $atts[ 'width' ] . '"' : ' width="100%"';
+	$iframe .= ( isset( $atts[ 'height' ] ) ) ? ' height="' . $atts[ 'height' ] . '"' : ' height="600px"';
 	$iframe .= '></iframe>';
+	
 	echo $iframe;
-
+	
+	if( count( $errors ) ){
+		echo"\n" . '<div class="error gcs_errors">' . "\n";
+		echo"<small><strong>Error";
+		if( count( $errors ) > 1 ) echo"s";
+		echo" in calendar configuration: </strong></small>";
+		if( count( $errors ) > 1 ) echo"<br />\n";
+		foreach( $errors as $error) echo"<small>" . $error . "</small><br />\n";
+		echo'</div><!--//.error gcs_errors-->' . "\n";
+	}
 	?>
 	***************
-	<iframe src="https://www.google.com/calendar/embed?title=*TITLE*&amp;height=500&amp;wkst=2&amp;src=9pk4g9evvbabravigk2ek4fius%40group.calendar.google.com&amp;color=%235C1158&amp;src=en.usa%23holiday%40group.v.calendar.google.com&amp;color=%23AB8B00&amp;ctz=America/Los_Angeles" style=" border:solid 1px #777 " width="800" height="500" frameborder="0" scrolling="no"></iframe>
+	<iframe src="https://www.google.com/calendar/embed?title=*TITLE*&amp;height=500&amp;wkst=2&amp;src=9pk4g9evvbabravigk2ek4fius%40group.calendar.google.com&amp;color=%235C1158&amp;src=en.usa%23holiday%40group.v.calendar.google.com&amp;color=%23FF0000&amp;ctz=America/Los_Angeles" style=" border:solid 1px #777 " width="800" height="500" frameborder="0" scrolling="no"></iframe>
 	
 	<?php
-	echo'<p>*' . get_option('timezone_string') . '*</p>';
+	//echo'<p>*' . get_option('timezone_string') . '*</p>';
+	echo'<p>Errors:' . print_r( $errors ) . '</p>';
 	$output = ob_get_contents();
 	ob_end_clean();
 	return $output;
