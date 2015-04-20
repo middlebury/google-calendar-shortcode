772fa255ae758a187fe33c817c8bf0cf5e1bac42
Eli Madden <emadden@middlebury.edu>
2015-04-20 13:37:29 -0400

    bgcolor, show_border

--- google-calendar-shortcode.php
+++ google-calendar-shortcode.php
@@ -55,35 +55,36 @@ function gcs_calendar( $atts ){
 		$iframe .= 'wkst=' . $weekstart . '&amp;';
 	}
 	if( isset( $atts[ 'language' ] ) && in_array( strtoupper( $atts[ 'language' ] ), $language_array ) ) $iframe .= 'hl=' . $atts[ 'language' ] . '&amp;';
+	if( isset( $atts[ 'bgcolor' ] ) ){
+		$color = trim( $atts[ 'bgcolor' ], " #\t\n\r\0\x0B" ); //strip off '#' as well as the usual space, etc
+		$color = str_replace( '%23', '', $color ); //if user copy-pasted the %23 from Google, strip it off as well
+		if( preg_match( '/^[a-f0-9]{3}$|^[a-f0-9]{6}$/i', $color ) ) $iframe .= 'bgcolor=%23' . $color . '&amp;'; //check to be sure it's a hex code. Otherwise don't use it and Google will default to white.		
+	}
 
 	$count = 0;
-	foreach( $id_array as $id ){
-		if( $count ) $iframe .= '&amp;';
-		
-		//****verify proper id****
-		$iframe .= 'src=' . trim( $id );
-		
+	foreach( $id_array as $id ){ //do each of the given calendar ID(s) and match with the given color(s)
+		if( $count ) $iframe .= '&amp;';		
+		$iframe .= 'src=' . trim( $id );		
 		if( isset( $color_array[ $count ] ) ){
 			$color = trim( $color_array[ $count ], " #\t\n\r\0\x0B" ); //strip off '#' as well as the usual space, etc
 			$color = str_replace( '%23', '', $color ); //if user copy-pasted the %23 from Google, strip it off as well
 			$color = ( preg_match( '/^[a-f0-9]{3}$|^[a-f0-9]{6}$/i', $color ) ) ? $color : '2952A3'; //check to be sure it's a hex code, if not use default Blue. *Note: Google at this time appears to only support color codes that match the choices they give.
 			$iframe .= '&amp;color=%23' . $color;
 		}
-
 		$count++;
 	}
 	$iframe .= '"';
 
+	if( isset( $atts[ 'show_border' ] ) && in_array( strtoupper( $atts[ 'show_border' ] ), array( '1', 'YES', 'TRUE' ) ) ) $iframe .= ' style=" border:solid 1px #777 "';
 	if( isset( $atts[ 'width' ] ) )$iframe .= ' width="' . $atts[ 'width' ] . '"';
 	if( isset( $atts[ 'height' ] ) ) $iframe .= ' height="' . $atts[ 'height' ] . '"';
-	if( isset( $atts[ 'viewmode' ] ) ) $iframe .= ' height="' . $atts[ 'height' ] . '"';
 
 	$iframe .= '></iframe>';
 	echo $iframe;
 
 	?>
 	***************
-	<iframe src="https://www.google.com/calendar/embed?title=*TITLE*&amp;showTz=0&amp;height=500&amp;wkst=2&amp;hl=ZH_CN&amp;bgcolor=%23ffffff&amp;src=9pk4g9evvbabravigk2ek4fius%40group.calendar.google.com&amp;color=%235C1158&amp;src=en.usa%23holiday%40group.v.calendar.google.com&amp;color=%23AB8B00&amp;ctz=America%2FNew_York" style=" border-width:0 " width="800" height="500" frameborder="0" scrolling="no"></iframe>
+	<iframe src="https://www.google.com/calendar/embed?title=*TITLE*&amp;showTz=0&amp;height=500&amp;wkst=2&amp;hl=tr&amp;bgcolor=%2355ff09&amp;src=9pk4g9evvbabravigk2ek4fius%40group.calendar.google.com&amp;color=%235C1158&amp;src=en.usa%23holiday%40group.v.calendar.google.com&amp;color=%23AB8B00&amp;ctz=America%2FNew_York" style=" border:solid 1px #777 " width="800" height="500" frameborder="0" scrolling="no"></iframe>
 	<?php
 	$output = ob_get_contents();
 	ob_end_clean();
